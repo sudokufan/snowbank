9f641cd7d9e876af2c071e5f8fda3c1b7dc5e37e
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 14:41:31 +0000

    added connecttoDB function and module.exported it

--- Services/DbService.js
+++ Services/DbService.js
@@ -2,3 +2,12 @@ const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017/'
 const dbname = 'squabank'
 const Client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const connectToDb = (cb) => {
+    Client.connect(() => {
+        let db = Client.db(dbname)
+        cb(db)
+    })
+}
+
+module.exports = connectToDb
