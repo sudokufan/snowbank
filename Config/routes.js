bc6265b930f5c2988778ca19a2346b4ddb8dc2ed
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 15:15:14 +0000

    create route for creating account

--- Config/routes.js
+++ Config/routes.js
@@ -1,3 +1,8 @@
+const AccountsController = require('../Controllers/AccountsController')
 
+const routes = (app) => {
+
+    app.post('/accounts', AccountsController.createAccount)
+}
 
 module.exports = routes
