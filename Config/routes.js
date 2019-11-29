f0476ed646113f0f464b91928fc1db8d6141551c
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 16:16:37 +0000

    added routes for new functions

--- Config/routes.js
+++ Config/routes.js
@@ -2,6 +2,10 @@ const AccountsController = require('../Controllers/AccountsController')
 
 const routes = (app) => {
 
+    app.get('/accounts/:balance/lessthan', AccountsController.retrieveAccountsUnderGivenAmount)
+
+    app.get('/accounts/:balance/morethan', AccountsController.retrieveAccountsOverGivenAmount)
+
     app.get('/accounts', AccountsController.getAccounts)
 
     app.post('/accounts', AccountsController.createAccount)
