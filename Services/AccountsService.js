e4f231cb74dce91a39a6e8ee57dfe0c0ca8015fe
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 15:28:53 +0000

    wrote createAccount function with module.export

--- Services/AccountsService.js
+++ Services/AccountsService.js
@@ -1,8 +1,14 @@
 const getAccounts = (db, callback) => {
-    var collection = db.collection('accounts')
+    let collection = db.collection('accounts')
     collection.find({}).toArray((err, docs) => {
         callback(docs)
     })
 }
 
+const createAccount = (db, newAccountInfo) => {
+    let collection = db.collection('accounts')
+    collection.insertOne(newAccountInfo)
+}
+
 module.exports.getAccounts = getAccounts
+module.exports.createAccount = createAccount
