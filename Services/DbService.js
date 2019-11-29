d248d381b3605947b1d88026ae1549236f44d5be
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 14:40:35 +0000

    finished mongoDB connection

--- Services/DbService.js
+++ Services/DbService.js
@@ -1,3 +1,4 @@
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017/'
 const dbname = 'squabank'
+const Client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true})
