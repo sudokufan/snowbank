3f34478f6b0a867eb2c6086b7ef7fc061746b2c3
sudokufan <alexrichardgeorge@protonmail.com>
2019-11-29 15:07:45 +0000

    added route for account functionality

--- Config/routes.js
+++ Config/routes.js
@@ -1,3 +1,8 @@
 const AccountsController = require('../Controllers/AccountsController')
 
+const routes = (app) => {
+
+    app.get('/accounts', AccountsController.getAccounts())
+}
+
 module.exports = routes
