5e2dc732b7ea03b5f334071b224c6a1fd7daf482
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-05 16:45:35 +0000

    changed port to 8000

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const routes = require('./Config/routes')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 
-app.use(cors())
 app.use(bodyParser.json())
 
 routes(app)
--- start.js
+++ start.js
@@ -1,4 +1,4 @@
 const app = require('./app')
-const PORT = 3000
+const PORT = 8000
 
 app.listen(PORT)
