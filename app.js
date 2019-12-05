7ec73725551b668a2355116e157b55a93a6aae07
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-05 13:36:11 +0000

    installed cors

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const routes = require('./Config/routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 routes(app)
